1d1b33b76e8965994e046037e9a10085102a7f1b
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-07 18:52:19 +0300

    Import a Component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    Hello world
+    <TodoApp />
   </div>
 </template>
 
 <script>
+import TodoApp from './components/TodoApp.vue'
 
+export default {
+  name: 'app',
+  components: {
+    TodoApp,
+  }
+}
 </script>
+
 
 <style>
 
