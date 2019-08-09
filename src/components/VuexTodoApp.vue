e27abba12fe25943f8808355bbe8ad7d227e6e88
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-09 17:50:30 +0300

    Get one and Post a new Todo using axios and Vuex

--- src/components/VuexTodoApp.vue
+++ src/components/VuexTodoApp.vue
@@ -2,17 +2,41 @@
   <div class="todos">
     <div class="heading">Read Todos from JSONPlaceholder (Using Vuex and axios)</div>
     <VuexTodoAppElement v-for="(todo, index) in allTodos" :key="index" :todo="todo"/>
+
+    <div class="heading">Fetch one todo</div>
+    <VuexTodoAppElement :todo="oneTodo"/>
+
+    <div class="heading">Create todo</div>
+    <form @submit.prevent>
+      <div class="form-group">
+        <input
+          type="text"
+          name="title"
+          id="title"
+          class="form-control"
+          placeholder="Add Todo ..."
+          v-model="title"
+          @keyup.enter="addTodo($event.target.value)"
+        >
+      </div>
+    </form>
   </div>
 </template>
   
 <script>
 import { mapGetters, mapActions } from "vuex";
 import VuexTodoAppElement from "./VuexTodoAppElement.vue";
+import uuid from "uuid";
 
 export default {
   name: "VuexTodoApp",
+  data() {
+    return {
+      title: ""
+    };
+  },
   computed: {
-    ...mapGetters(["allTodos"])
+    ...mapGetters(["allTodos", "oneTodo"])
   },
 
   components: {
@@ -20,11 +44,20 @@ export default {
   },
 
   methods: {
-    ...mapActions(["fetchAllTodos"])
+    ...mapActions(["fetchAllTodos", "fetchOneTodo", "postTodo"]),
+    addTodo(value) {
+      this.title = value;
+      this.postTodo({
+        id: uuid.v4(),
+        title: this.title,
+        completed: false
+      });
+    }
   },
 
   created() {
     this.fetchAllTodos();
+    this.fetchOneTodo(1);
   }
 };
 </script>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,35 +1,56 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-const API = 'https://jsonplaceholder.typicode.com/todos?_limit=5'
+const API = 'https://jsonplaceholder.typicode.com/todos/'
+
+const showError = (message) => Vue.notify({
+  group: 'notifications-group',
+  type: 'error',
+  duration: 3000,
+  title: 'Error',
+  text: message
+})
 
 export default {
   state: {
-    todos: []
+    todos: [],
+    todo: {}
   },
 
   getters: {
-    allTodos: state => state.todos
+    allTodos: state => state.todos,
+    oneTodo: state => state.todo
   },
 
   mutations: {
-    setAllTodos: (state, payload) => state.todos = payload
+    setAllTodos: (state, payload) => state.todos = payload,
+    setOneTodo: (state, payload) => state.todo = payload,
+    createTodo: (state, payload) => {
+      state.todos.unshift(payload)
+      state.todo = payload
+    }
   },
 
   actions: {
     fetchAllTodos: async ({ commit }) => {
-      await axios.get(API).then((response) => {
-        commit('setAllTodos', response.data)
-      }).catch((error) => {
-        Vue.notify({
-          group: 'notifications-group',
-          type: 'error',
-          duration: 3000,
-          title: 'Error',
-          text: error.message
-        })
-      })
+      await axios.get(API + '?_limit=10')
+        .then((response) => {
+          commit('setAllTodos', response.data)
+        }).catch((error) => showError(error.message))
+    },
+
+    fetchOneTodo: async ({ commit }, id) => {
+      await axios.get(API + id.toString())
+        .then((response) => {
+          commit('setOneTodo', response.data)
+        }).catch(error => showError(error.message))
+    },
+
+    postTodo: async ({ commit }, data) => {
+      await axios.post(API, data)
+        .then((response) => {
+          commit('createTodo', response.data)
+        }).catch((error) => showError(error.message))
     }
   },
-
 }
